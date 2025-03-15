fe4cef70b9e7ddc3bef16569f25ba51dc17568f3
Xsitsu <jacoblocke21@live.com>
2025-03-15 21:38:04 +0000

    Basic request and beautifulsoup parser.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 
-print("hello world!")
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    tables = soup.find_all('table')
+    for table in tables:
+        if table.has_attr('class') and table['class'][0] == 'roundy':
+            print("============================================")
+            print("Entry:", table)
+            print("")
+            print("")
+
+else:
+    print("Get failed with status code: ", response.status_code)
+
